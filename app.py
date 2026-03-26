fc7e71c9c0a7fa43182967f4dd411d1d5660b495
mayurkamane19 <mayurkamane19@gmail.com>
2026-03-26 14:24:08 +0530

    fix double run issue

--- app.py
+++ app.py
@@ -95,4 +95,9 @@ def update(id):
     return redirect("/")
 
 # ---------- RUN ----------
-app.run(debug=True)
+# ---------- RUN ----------
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
